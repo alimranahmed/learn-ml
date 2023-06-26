71cf16e70c77450e0254004f29af8208074ff357
Al Imran Ahmed <al.imran.cse@gmail.com>
2023-06-27 00:50:02 +0200

    env removed

--- env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/al_imran/Projects/personal/learn-ml/env/lib"
-XSLT_LIBS="-lxslt -L/Users/al_imran/Projects/personal/learn-ml/env/lib -lxml2 "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/al_imran/Projects/personal/learn-ml/env/include"
-MODULE_VERSION="xslt-1.1.37"
